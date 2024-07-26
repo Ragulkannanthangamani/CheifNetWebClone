bce3c8ce639e871e556d6924490cd252f32cf087
Ragulkannanthangamani <ragulkannanthangamani@gmail.com>
2024-07-26 09:09:32 +0530

    organization UI

--- src/app/organization/add-organization/add-organization.component.ts
+++ src/app/organization/add-organization/add-organization.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-organization',
   templateUrl: './add-organization.component.html',
   styleUrl: './add-organization.component.css'
 })
-export class AddOrganizationComponent {
+export class AddOrganizationComponent implements OnInit {
+  userform:FormGroup
+  constructor(private fb:FormBuilder){
+    this.userform=this.fb.group({
+      name :['',Validators.required],
+      Description:['',Validators.required],
+      UUID:['',Validators.required],
+      ClientEmail:['',Validators.required],
+      ClientMobileno:['',Validators.required],
+      ClientName:['',Validators.required],
+      ClientAddress:['',Validators.required],
+      PrismaFirewall:['',Validators.required]
+  })
+  console.log(this.userform);
+  
+  }
+  ngOnInit(): void {
+    
+  }
 
 }
